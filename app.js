16cdb0ef5ea74055d281eddad954534180771488
Singal <tssingal@147dda08c88f.ant.amazon.com>
2021-03-01 17:55:05 +0530

    Updated package.json and .gitignore

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://${process.env.CLUSTER_USERNAME}:${process.env.CLUSTER_PASSWORD}@chatappcluster.txlsi.mongodb.net/chatappDb?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+let uri = `mongodb+srv://${process.env.CLUSTER_USERNAME}:${process.env.CLUSTER_PASSWORD}@chatappcluster.txlsi.mongodb.net/chatappDb?retryWrites=true&w=majority`;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
     name: String,
